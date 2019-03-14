e51ec84a91598da86428cad0417e7e6ba21ae569
Angelo Cordon <angelocordon@gmail.com>
2019-03-14 11:58:26 -0700

    Refactor Dashboard component
    Move Dashboard component to folder to refactor paths / routes

--- imports/ui/components/dashboard/index.jsx
+++ imports/ui/components/dashboard/index.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
-import { Container, Row, Col, ListGroup } from 'react-bootstrap';
-// import BoardContainer from '/imports/ui/containers/board.jsx';
-// import '/imports/ui/styles/_dashboard.scss';
+import { Container, Row, Col, CardDeck, Card } from 'react-bootstrap';
 import DashboardSidebar from './dashboard_sidebar';
 
 const Dashboard = props => {
-  const { user } = props;
-  const { name } = user && user.profile || {}
-  
+
   return (
     <Container>
       <Row>
-        <DashboardSidebar></DashboardSidebar>
+        <Col sm='3'>
+          <DashboardSidebar></DashboardSidebar>
+        </Col>
+        
         <Col>
-          <p className='font-weight-bold'>
-            Your Current Matches
-          </p>
+          {/* Your Current Matches */}
+
+          {/* Accordions:
+          + Mentors
+          + Mentees
+          + OSS Projects
+          + Contributors
+          + Accountabili-buddies
+					Other */}
+					
+          {/* Mentors */}
+          <section className='dashboard-card-section'>
+            <h1 className='h5 text-capitalize font-weight-normal'>
+              People looking for mentors
+            </h1>
+
+            <CardDeck className='d-block'>
+              <Card style={{ width: '33%' }}>
+                <Card.Body>
+                  Hello
+                </Card.Body>
+              </Card>
+            </CardDeck>					
+          </section>
         </Col>
       </Row>
     </Container>
