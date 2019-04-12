08deedae744c08e68aeb242c80ce52d5f526f7f2
Angelo Cordon <angelocordon@gmail.com>
2019-04-12 01:40:56 -0700

    Update DashboardSidebar Spec
    Add additional documentation.
    Add a pending test to test handlers and passing correct item keys. This is
    important eventually, but difficult to figure out currently.

--- specs/components/dashboard/dashboard_sidebar.spec.js
+++ specs/components/dashboard/dashboard_sidebar.spec.js
@@ -1,13 +1,14 @@
 import React from 'react';
+// NOTE: We use `mount` instead of `shallow` because we need to render
+// React-Bootstrap components within the DashboardSidebar component.
 import { mount } from 'enzyme';
-
 import { categories } from 'lib/data/categories';
 import DashboardSidebar from 'components/dashboard/dashboard_sidebar';
 
+const validCategories = categories.map(category => category.short_label);
+
 describe('<DashboardSidebar />', () => {
   it('should render a list of valid category items', () => {
-    const validCategories = categories.map(category => category.short_label);
-
     // TODO: `DashboardSidebar component should have validCategories as
     // default props, not as props (sections) passed through
     const wrapper = mount(<DashboardSidebar sections={validCategories} />);
@@ -16,4 +17,6 @@ describe('<DashboardSidebar />', () => {
       expect(wrapper.containsMatchingElement(<button>{label}</button>)).toBeTruthy();
     }
   });
+
+  test.todo('passing the correct item key to onVisibilityChange prop');
 });
