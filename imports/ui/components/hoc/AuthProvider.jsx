bfb943262c9511fd06e46f7712d422446f394815
Roberto Quezada <sgtquezada@gmail.com>
2019-04-23 10:23:15 -0600

    resolve lint and prettier errors

--- imports/ui/components/hoc/AuthProvider.jsx
+++ imports/ui/components/hoc/AuthProvider.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-const AuthContext = React.createContext()
+const AuthContext = React.createContext();
 
 class AuthProvider extends React.Component {
   constructor() {
-    super()
+    super();
   }
   state = {
-    user: null
-    }
+    user: null,
+  };
 
   componentDidMount() {
-    Accounts.onLogout(_ => this.setState({ user: null }))
-    Accounts.onLogin(_ => this.setState({ user: Meteor.user() }))
+    Accounts.onLogout(_ => this.setState({ user: null }));
+    Accounts.onLogin(_ => this.setState({ user: Meteor.user() }));
   }
 
   render() {
-    return (
-      <AuthContext.Provider
-        value={{ user: this.state.user }}
-      >
-      {this.props.children}
-      </AuthContext.Provider>
-    )
+    return <AuthContext.Provider value={{ user: this.state.user }}> {this.props.children} </AuthContext.Provider>;
   }
 }
 
-const AuthConsumer = AuthContext.Consumer
-export { AuthProvider, AuthConsumer, AuthContext }
+const AuthConsumer = AuthContext.Consumer;
+export { AuthProvider, AuthConsumer, AuthContext };
--- imports/ui/components/hoc/AuthenticatedRoute.jsx
+++ imports/ui/components/hoc/AuthenticatedRoute.jsx
@@ -1,18 +1,11 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import { AuthConsumer } from './AuthProvider'
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthConsumer } from './AuthProvider';
 
 const AuthenticatedRoute = ({ component: Component, ...rest }) => (
   <AuthConsumer>
-    {({ user }) => (
-      <Route
-        render={props =>
-          user ? <Component {...props} /> : <Redirect to="/login" />
-        }
-        {...rest}
-      />
-    )}
+    {({ user }) => <Route render={props => (user ? <Component {...props} /> : <Redirect to="/login" />)} {...rest} />}
   </AuthConsumer>
-)
+);
 
-export default AuthenticatedRoute
+export default AuthenticatedRoute;
