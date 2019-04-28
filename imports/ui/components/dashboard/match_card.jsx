8ebba303d2698d0a66ee107eb31fbee8a0426e6a
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2019-04-28 00:14:21 -0700

    Remove unused  CardDeck component and add missing semicolon

--- imports/ui/components/dashboard/match_card.jsx
+++ imports/ui/components/dashboard/match_card.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import { Card, CardDeck, Dropdown, ButtonGroup, Button, Modal, Form } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Card, Dropdown, ButtonGroup, Button, Modal, Form } from 'react-bootstrap';
 import '/imports/ui/styles/_cards.scss';
 import { PropTypes } from 'prop-types';
 
 class MatchCard extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.showFlagModal = this.showFlagModal.bind(this);
     this.closeFlagModal = this.closeFlagModal.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.submitFlagModal = this.submitFlagModal.bind(this);
     this.state = {
       showFlagModal: false,
-      flagReason: ''
+      flagReason: '',
     };
   }
   showFlagModal(event) {
@@ -25,26 +25,26 @@ class MatchCard extends Component {
   }
   handleChange(e) {
     this.setState({
-      flagReason: e.target.value
+      flagReason: e.target.value,
     });
   }
   submitFlagModal(e) {
     e.preventDefault();
 
     const data = {
-      "userId": Meteor.userId(),
-      "reason": this.state.flagReason,
-      "entryId": this.props.entryId
-    }
+      userId: Meteor.userId(),
+      reason: this.state.flagReason,
+      entryId: this.props.entryId,
+    };
 
     this.setState({ showFlagModal: false });
-    Meteor.call("entry.flag", data, (error, result) => {
+    Meteor.call('entry.flag', data, (error, result) => {
       if (error) {
-        this.setState({ 'error' : error.reason });
-        this.setState({ 'processing' : false });
+        this.setState({ error: error.reason });
+        this.setState({ processing: false });
       }
       if (result) {
-        this.setState({ 'processing' : false });
+        this.setState({ processing: false });
       }
     });
   }
@@ -53,52 +53,62 @@ class MatchCard extends Component {
     const { oneLineIntro, lookingFor, ownCard, timezone } = this.props;
     return (
       <div>
-      <Card>
-        <Card.Body>
-          <Card.Title className="font-weight-normal">{oneLineIntro}</Card.Title>
-          <Card.Text>{lookingFor}</Card.Text>
-          {ownCard === 'true' ? (
-            ''
-          ) : (
-            <Dropdown as={ButtonGroup} className="btn-block">
-              <Button className="btn-block" variant="primary">
-                Request Match
+        <Card>
+          <Card.Body>
+            <Card.Title className="font-weight-normal">{oneLineIntro}</Card.Title>
+            <Card.Text>{lookingFor}</Card.Text>
+            {ownCard === 'true' ? (
+              ''
+            ) : (
+              <Dropdown as={ButtonGroup} className="btn-block">
+                <Button className="btn-block" variant="primary">
+                  Request Match
+                </Button>
+
+                <Dropdown.Toggle split variant="primary" />
+
+                <Dropdown.Menu>
+                  <Dropdown.Item as="button" onClick={this.showFlagModal}>
+                    Flag
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">{timezone}</small>
+          </Card.Footer>
+        </Card>
+
+        <Modal show={this.state.showFlagModal} onHide={this.closeFlagModal}>
+          <Form noValidate onSubmit={e => this.submitFlagModal(e)}>
+            <Modal.Header closeButton onClick={this.closeFlagModal}>
+              <Modal.Title>Why are you flagging this entry?</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Form.Group controlId="flagReason">
+                <Form.Control
+                  type="text"
+                  defaultValue={this.state.flagReason}
+                  onChange={this.handleChange}
+                  placeholder="Enter reason"
+                />
+              </Form.Group>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.closeFlagModal}>
+                Close
               </Button>
-
-              <Dropdown.Toggle split variant="primary" />
-
-              <Dropdown.Menu>
-                <Dropdown.Item as='button' onClick={this.showFlagModal}>Flag</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">{timezone}</small>
-        </Card.Footer>
-      </Card>
-      
-      <Modal show={this.state.showFlagModal} onHide={this.closeFlagModal}>
-      <Form noValidate onSubmit={e => this.submitFlagModal(e)}>
-        <Modal.Header closeButton onClick={this.closeFlagModal}>
-          <Modal.Title>Why are you flagging this entry?</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <Form.Group controlId="flagReason">
-            <Form.Control type="text" defaultValue={this.state.flagReason} onChange={this.handleChange} placeholder="Enter reason" />
-          </Form.Group>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.closeFlagModal}>Close</Button>
-          <Button variant="primary" onClick={this.submitFlagModal}>Submit</Button>
-        </Modal.Footer>
-        </Form>
-      </Modal>
-      
-    </div>
-    )
+              <Button variant="primary" onClick={this.submitFlagModal}>
+                Submit
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+      </div>
+    );
   }
 }
 
