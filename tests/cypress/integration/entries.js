98bd5a9247f1a2c75bfb14dd5f7e4e052b40e5c2
sebbel <seb.pancia@gmail.com>
2019-04-28 07:56:11 +0200

    Setting up cypress for cb-connect along with a first test

---
+++ tests/cypress/integration/entries.js
@@ -0,0 +1,18 @@
+describe('Entries', function() {
+  it('.should be able to be added through the form', function() {
+    cy.visit('http://localhost:3000/apply');
+
+    // Click the button
+    cy.get('form #3').click();
+    cy.get('form button[type="submit"]').click();
+
+    cy.get('form #name').type('Codebuddy');
+    cy.get('form #oneLineIntro').type('I am a codebuddy from an automated test');
+    cy.get('form #lookingFor').type('I am looking for people who help me get better at what I do.');
+    cy.get('form button[type="submit"]').click();
+
+    cy.get('form #email').type('codebuddy@codebuddies.org');
+    cy.get('form #timezone').select('12');
+    cy.get('form button[type="submit"]').click();
+  });
+});
