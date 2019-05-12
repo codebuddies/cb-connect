1500de1a897a8b2e1dc9d80a30c5c8b4bc51d1bc
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2019-05-12 15:51:26 -0400

    Add build and deploy script

---
+++ scripts/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+root_directory=`git rev-parse --show-toplevel 2>/dev/null`
+
+commitHash=$(git rev-parse HEAD)
+echo "Commit hash: $commitHash"
+
+echo -e "\nInstalling dependencies"
+meteor npm install --production
+
+echo -e "Building app"
+mkdir  -p $root_directory/build/
+meteor build --directory $root_directory/build/ --allow-superuser
+
+sleep 2
+
+cd  $root_directory/build/bundle/programs/server/ && meteor npm install
+mkdir -p /cb-connect/bundles/$commitHash
+mv $root_directory/build/bundle/* /cb-connect/bundles/$commitHash
+cp $root_directory/conf/dev-settings.json /cb-connect/bundles/$commitHash/dev-settings.json
+rm -rf $root_directory/build
+rm -rf $root_directory/.meteor/local
+rm -rf $root_directory/node_modules
+
+
+echo -e "\n\nBundle created successfully! Run following command to deploy it:"
+echo "./deploy.sh $commitHash"
+
+echo -e "\nRUN TIME: $(($SECONDS / 60))m $(($SECONDS % 60))s"
