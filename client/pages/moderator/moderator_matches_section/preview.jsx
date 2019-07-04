328a28ee1bb797a0bf781f22546526f8754af865
ISPOL <insignificantpointoflight@gmail.com>
2019-07-04 16:55:16 -0400

    added names

--- client/pages/moderator/moderator_matches_section/preview.jsx
+++ client/pages/moderator/moderator_matches_section/preview.jsx
@@ -9,7 +9,7 @@ class PreviewEmail extends Component {
     super(props);
     this.matchUsers = this.matchUsers.bind(this);
     this.createEmail = this.createEmail.bind(this);
-    this.state = {};
+
     //get user IDs of the two entries we want to match
     const getUserIds = [];
     this.props.cardsSelected.forEach(card => {
@@ -30,75 +30,80 @@ class PreviewEmail extends Component {
       }
     });
   }
-  createEmail(userData) {
-    const user1 = userData[0];
-    const user2 = userData[1];
+  createEmail(userData, userInfo) {
+    if (userInfo.length > 0) {
+      const user1 = userData[0];
+      const user2 = userData[1];
+      let userInfo1 = userInfo[0]._id === user1.userId ? userInfo[0] : userInfo[1];
+      let userInfo2 = userInfo[1]._id === user2.userId ? userInfo[1] : userInfo[0];
+      return (
+        <div className="email">
+          <h4>Hey {userInfo1.profile.name},</h4>
 
-    return (
-      <div className="email">
-        <h4>Hey {user1.userId},</h4>
+          <p>
+            After sifting through all of the submissions, we narrowed down the perfect {user1.category.title} for you
+            this week.
+          </p>
 
-        <p>
-          After sifting through all of the submissions, we narrowed down the perfect {user1.category.title} for you this
-          week.
-        </p>
+          <p>
+            People who have connected through CodeBuddies have gone on to make projects together, host hangouts
+            together, and keep each other accountable to their goals.
+          </p>
 
-        <p>
-          People who have connected through CodeBuddies have gone on to make projects together, host hangouts together,
-          and keep each other accountable to their goals.
-        </p>
+          <p>As a reminder, you said you were looking for:</p>
 
-        <p>As a reminder, you said you were looking for:</p>
+          <h5>{user2.category.title}</h5>
+          <ul>
+            <li>
+              <strong>One-line intro of yourself:</strong> {user1.oneLineIntro}
+            </li>
+            <li>
+              <strong>What skills can you help others with?</strong> {user1.skillHelpOther}
+            </li>
+            <li>
+              <strong>What skills are you trying to improve?</strong> {user1.skillImproveSelf}
+            </li>
+            <li>
+              <strong>Timezone:</strong> {user1.tz.title}
+            </li>
+          </ul>
 
-        <h5>{user2.category.title}</h5>
-        <ul>
-          <li>
-            <strong>One-line intro of yourself:</strong> {user1.oneLineIntro}
-          </li>
-          <li>
-            <strong>What skills can you help others with?</strong> {user1.skillHelpOther}
-          </li>
-          <li>
-            <strong>What skills are you trying to improve?</strong> {user1.skillImproveSelf}
-          </li>
-          <li>
-            <strong>Timezone:</strong> {user1.tz.title}
-          </li>
-        </ul>
-
-        <h5>Your CodeBuddy is {user2.userId}.</h5>
-        <p>
-          {user2.userId} is looking for {user1.category.title}
-        </p>
-        <ul>
-          <li>
-            <strong>One-line intro of {user2.userId}:</strong> {user2.oneLineIntro}
-          </li>
-          <li>
-            <strong>Skill(s) {user2.userId} can help others with:</strong> {user2.skillHelpOther}
-          </li>
-          <li>
-            <strong>Skill(s) {user2.userId} is trying to improve:</strong> {user2.skillImproveSelf}
-          </li>
-          <li>
-            <strong>Timezone:</strong> {user2.tz.title}
-          </li>
-        </ul>
-      </div>
-    );
+          <h5>
+            <strong>Your CodeBuddy is</strong> {userInfo2.profile.name}.
+          </h5>
+          <p>
+            {user2.userId} is looking for {user1.category.title}
+          </p>
+          <ul>
+            <li>
+              <strong>One-line intro of {user2.userId}:</strong> {user2.oneLineIntro}
+            </li>
+            <li>
+              <strong>Skill(s) {user2.userId} can help others with:</strong> {user2.skillHelpOther}
+            </li>
+            <li>
+              <strong>Skill(s) {user2.userId} is trying to improve:</strong> {user2.skillImproveSelf}
+            </li>
+            <li>
+              <strong>Timezone:</strong> {user2.tz.title}
+            </li>
+          </ul>
+        </div>
+      );
+    }
   }
 
   render() {
     const { cardsSelected, findUsersToMatch } = this.props;
-
+    console.log(findUsersToMatch);
     return (
       <div className="container" id="preview_email">
         <div className="row">
           <h2 className="center">Preview Emails</h2>
         </div>
 
-        {this.createEmail(cardsSelected)}
-        {this.createEmail([cardsSelected[1], cardsSelected[0]])}
+        {this.createEmail(cardsSelected, findUsersToMatch)}
+        {this.createEmail([cardsSelected[1], cardsSelected[0]], findUsersToMatch)}
 
         <Button onClick={this.matchUsers}>Match Users</Button>
       </div>
