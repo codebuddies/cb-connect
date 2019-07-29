a0aec4cce91da05e01a86da77abaa6176a5e314e
Julius Alvarado <javascript.uiux@gmail.com>
2019-07-29 04:26:11 -0700

    a bit of php code added and the api micro framework readme

---
+++ api/php-sql/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ *
+ */
+
+ echo "Hello World ^_^/"; 
+
+
+ echo "future cb-connect PHP 7 & SQL API"; 
+
+
+
+
+
+
+
+
+
+
+// end of php file
