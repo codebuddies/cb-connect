2d099279606827ddb2fd622447e45bb5beccdcc5
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2019-08-19 21:28:09 -0700

    Fixed problem with user props being null, causing AuthenticatedRoute to redirect to the Login component every time. Also imported Container from react-bootstrap into the profile page.

--- client/hoc/AuthenticatedRoute.jsx
+++ client/hoc/AuthenticatedRoute.jsx
@@ -5,7 +5,7 @@ import { PropTypes } from 'prop-types';
 
 const AuthenticatedRoute = ({ component: Component, ...rest }) => (
   <AuthConsumer>
-    {({ user }) => <Route render={props => (user ? <Component {...props} /> : <Redirect to="/login" />)} {...rest} />}
+    {user => <Route render={props => (user ? <Component {...props} /> : <Redirect to="/login" />)} {...rest} />}
   </AuthConsumer>
 );
 
--- client/pages/user/my_profile.jsx
+++ client/pages/user/my_profile.jsx
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import { AuthContext } from '../../hoc/AuthProvider';
+import { Container } from 'react-bootstrap';
 
 class MyProfile extends Component {
-
-  render () {
+  render() {
     const { user } = this.context;
     console.log('MyProfile render');
     return (
       <Container fluid>
         <h2 className="text-center">profile</h2>
       </Container>
-    )
-
+    );
   }
-
-};
-
+}
 
 MyProfile.contextType = AuthContext;
 export default MyProfile;
