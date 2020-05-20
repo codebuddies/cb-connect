81b48e33a851bb206d80f65ed052b689e5908a61
Julius Alvarado <javascript.uiux@gmail.com>
2020-05-20 02:32:37 -0700

    app will check if its' in debug mode or a local env, will probably need to more
    rigorously test this but so far It's been working great! I could just start the debugger or deploy it and not have to manually change anything :)

--- api/php/src/classes/AppGlobals.php
+++ api/php/src/classes/AppGlobals.php
@@ -1,7 +1,5 @@
 <?php
-
 declare(strict_types=1);
-
 /**
  * Created by PhpStorm.
  * User: julius
@@ -14,54 +12,25 @@ namespace CodeBuddies;
 
 class AppGlobals
 {
-    //-------------------------------------------------------------
-    // TURN DEBUG MODE on OR off, then set the route in routes.php
-    public static $NINJA_AUTO_DEBUG = true;
-    //-------------------------------------------------------------
-    
-    //-------------------------------------------------------------
-    // TURN TEST MODE on OR off, this is set for unit testing
-    public static $NINJA_AUTO_TEST = false;
-    //-------------------------------------------------------------
-    
-    // local path, NOT the production path
-    private static $LogFolderPath = 'C:\xampp\htdocs\ninja\app\logs\\';
-    public static $accounting_csv = 'accounting-php.csv';
-    public static $coordinator_csv = 'coordinator-php.csv';
-    
-    public static function PathToFolder() {
-        return (gethostname() === 'Julius1')
-            ? 'C:\xampp\htdocs\ninja\app\commission-csv'
-            : 'C:\inetpub\wwwroot\ninja\app\commission-csv';
+    /**
+     * Determine if app is in local or remote environment
+     * @return bool
+     */
+    public static function isLocal(): bool {
+        $serverName = $_SERVER['SERVER_NAME'] ?? null;
+        // if app is not in CLI mode or server name is both not null and equal to local ip
+        return (PHP_SAPI == 'cli' || (!is_null($serverName) && $serverName == '10.0.0.210'));
     }
     
-    public static function LogInfo(string $info): void {
-        // fopen(), fwrite(), fclose()
-        $handle = null;
-        $newLines = "\n\r\n\r";
-        $info = "The accounting and coordinator data are different sizes ~routes.php line 333 ish";
-        $info = substr_replace($info, $newLines, 0, 0);
-        $info = substr_replace($info, $newLines, strlen($info), 0);
-        
-        // append all logs by day to the same file
-        $date = getdate();
-        $logDay = "COM_AUTO_LOG - $date[month] $date[mday], $date[year]";
-        $filePath = self::$LogFolderPath . $logDay . '.txt';
-        
-        // using file_exists() may be better
-        try {
-            // file already exists so just append to it.
-            $handle = fopen($filePath, 'a') or false;
-            fwrite($handle, $info);
-        }
-        catch(\Exception $e) {
-            // create the file then write to it
-            $handle = fopen($filePath, 'w') or exit("File at $filePath could not be created ~AppGlobals.php line 36 ish");
-            fwrite($handle, $info);
-        }
-        finally {
-            fclose($handle);
+    /**
+     * @return bool
+     */
+    public static function inDebugMode(): bool {
+        $getDebug = $_GET['debug'] ?? null;
+        if(PHP_SAPI == 'cli' || (!is_null($getDebug) && $getDebug == 'true')) {
+            return true;
         }
         
-    } // END OF: LogComAutoInfo()
+        return false;
+    }
 }
