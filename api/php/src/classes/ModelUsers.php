d3ba1ff23ddcba1762c65e35f6497ae6fe55f9d2
Julius Alvarado <javascript.uiux@gmail.com>
2020-05-21 08:40:42 -0700

    SQL injections should get sanitized, starting to implement matchSkills()

--- api/php/src/classes/ModelUsers.php
+++ api/php/src/classes/ModelUsers.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 
 namespace CodeBuddies;
 
+use Spatie\Regex\Regex;
+
 class ModelUsers
 {
     use CodeBuddiesUtil;
@@ -28,6 +30,10 @@ class ModelUsers
         $this->db = $dbCodeBuddiesConnect;
     }
     
+    /**
+     * The primary data I'll be using to implement the match will be against the mock users
+     * @return array
+     */
     public function getMockUsers(): array {
         try {
             $query = /** @lang */
@@ -45,27 +51,31 @@ class ModelUsers
         }
     }
     
+    /**
+     * Basically just add random skills to mock users to have data to work with
+     * @return array|string[]
+     */
     public function addSkillsToMockUsers(): array {
         try {
             // get id's
-            $query = /** @lang **/
+            $query = /** @lang * */
                 "select q_id from $this->tableMockUsers";
             $statement = $this->db->prepare($query);
             $statement->execute();
             $result = $statement->fetchAll();
-    
-            $queryFormat = /** @lang **/
+            
+            $queryFormat = /** @lang * */
                 "update %s set skills = '%s' where q_id = %d;";
             foreach($result as $i => $q_id) {
                 $randomSkills = $this->createRandomSkills(rand(2, 10));
                 $randomSkillsImplode = implode(", ", $randomSkills);
                 $q_id = (int)$q_id['q_id'];
                 $debug = 1;
-                $query = sprintf($queryFormat,  $this->tableMockUsers, $randomSkillsImplode, $q_id);
+                $query = sprintf($queryFormat, $this->tableMockUsers, $randomSkillsImplode, $q_id);
                 $statement = $this->db->prepare($query);
                 $statement->execute();
             }
-                
+            
             return ['x-cb-status' => '_> CB_CONNECT: added skills to mock users table'];
         }
         catch(\Throwable $e) {
@@ -75,5 +85,40 @@ class ModelUsers
                 'x-cb-info' => "_> CB_CONNECT Error: Query to get mock users table failed ~$ml",
             ];
         }
+    }
+    
+    /**
+     * The most important and difficult to implement match
+     *
+     * @param array $data
+     *
+     * @return array|string[]
+     */
+    public function matchSkills(array $data): array {
+        $data = $this->sanitizeData($data);
+        return ['x-cb-status' => '_> it worked ^_^'];
+    }
+    
+    /**
+     * Only accept comma's and alphanumeric chars. For security.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    private function sanitizeData(array $data): array {
+        $sanitizedData = [];
+        
+        // utility sanitize lambda
+        $sanLambda = function($i): string {
+            return Regex::replace('/[^\w,]/', '', $i)->result();
+        };
+        
+        foreach($data as $k => $datum) {
+            $sanitizedData[$sanLambda($k)] = $sanLambda($datum);
+        }
+        
+        $debug = 1;
+        return $sanitizedData;
     }
 }
--- api/php/src/routes.php
+++ api/php/src/routes.php
@@ -8,8 +8,8 @@ use CodeBuddies\ModelUsers;
 
 if(AppGlobals::inDebugMode()) {
     echo "\n <br> [ CODE BUDDIES CONNECT IN DEBUG MODE ] <br> \n";
-    $_SERVER['REQUEST_URI'] = '/test/add-skills';
-    $_SERVER['REQUEST_METHOD'] = 'GET';
+    $_SERVER['REQUEST_URI'] = '/test/get-matched';
+    $_SERVER['REQUEST_METHOD'] = 'POST';
 }
 
 $app->get('/hello/{name}',
@@ -17,6 +17,7 @@ $app->get('/hello/{name}',
         $zName = $args['name'];
         $response->getBody()->write("Well, Ello There $zName");
         // $this->logger->addInfo("routes.php julius alvarado logged something");
+        
         return $response;
     }
 );
@@ -26,7 +27,7 @@ $app->get('/test1',
         $dbCodeBuddiesConnect = AppGlobals::isLocal() ? $this->dbLocal : $this->dbProduction;
         $usersModel = new ModelUsers($dbCodeBuddiesConnect);
         $mockUsers = $usersModel->getMockUsers();
-    
+        
         // construct a table real quick
         $table = "<table><tr><th>first_name</th><th>last_name</th><th>user_type</th><th>gender</th></tr>";
         foreach($mockUsers as $user) {
@@ -43,17 +44,49 @@ $app->get('/test1',
 
 
 $app->get('/user',
-    function (Request $request, Response $response) {
+    function(Request $request, Response $response) {
         return $response->getBody()->write("Hello, user!");
     }
 );
 
 $app->get('/test/add-skills',
-    function (Request $request, Response $response) {
+    function(Request $request, Response $response) {
         //TODO: look into maybe creating a singleton for classes that are used often
         $dbCodeBuddiesConnect = AppGlobals::isLocal() ? $this->dbLocal : $this->dbProduction;
         $usersModel = new ModelUsers($dbCodeBuddiesConnect);
         $result = $usersModel->addSkillsToMockUsers();
+        
+        return $response->withJson($result);
+    }
+);
+
+$app->get('/test/get-matched',
+    function(Request $request, Response $response, array $args) {
+        return $this->renderer->render($response, 'get-matched.phtml', $args);
+    }
+);
+
+$app->post('/test/get-matched',
+    function(Request $request, Response $response) {
+        //$getParsedBody = var_export($data, true);
+        //file_put_contents('logs/parsed-body.txt', $getParsedBody);
+        //-- example parsed body for debugging --
+        $mockData = [
+            // mock a basic sql injection
+            'user-name' => "'SELECT * FROM users_db --",
+            // will result to = "SELECTFROMusers_dbjulius" after sanitation
+            'user-skills' => 'math, css, javascript, php',
+            'user-about' => '',
+            'app-name' => 'Code Buddies Connect',
+        ];
+        
+        // The DATA ^_^
+        $data = AppGlobals::inDebugMode() ? $mockData : $request->getParsedBody();
+        
+        //TODO: look into maybe creating a singleton for classes that are used often
+        $dbCodeBuddiesConnect = AppGlobals::isLocal() ? $this->dbLocal : $this->dbProduction;
+        $usersModel = new ModelUsers($dbCodeBuddiesConnect);
+        $result = $usersModel->matchSkills($data);
         
         return $response->withJson($result);
     }
@@ -66,8 +99,6 @@ $app->get('/[{name}]',
         return $this->renderer->render($response, 'index.phtml', $args);
     }
 );
-
-
 
 
 //--------------------------------------------------------------
