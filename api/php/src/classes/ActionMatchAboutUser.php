5e549881f16231230be881e6fab0db83568e9437
Julius Alvarado <javascript.uiux@gmail.com>
2020-05-23 18:08:37 -0700

    deleted schema-entry.js, added action classes for future implementation

---
+++ api/php/src/classes/ActionMatchAboutUser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 5/22/2020
+ * Time: 11:29 AM
+ */
+
+namespace CodeBuddies;
+
+
+class ActionMatchAboutUser
+{
+    
+}
---
+++ api/php/src/classes/ActionMatchLookingFor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 5/22/2020
+ * Time: 11:30 AM
+ */
+
+namespace CodeBuddies;
+
+
+class ActionMatchLookingFor
+{
+    
+}
